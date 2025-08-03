dbaa25e5ab6134bee6027939d8761db7ecfd6747
AnoshinaEvGen <mega-feya@mail.ru>
2025-08-03 21:02:14 +0300

    Созданы функции, которые обновляют названия выбранных валют в интерфейсе. Создана функция exchange для получения выбранных кодов криптовалют и валют с отработкой исключений.

--- IAR.py
+++ IAR.py
@@ -3,14 +3,41 @@ from tkinter import messagebox as mb
 import requests
 import json
 
+def update_b_label(event):
+    # Получаем полное название базовой криптовалюты из словаря
+    code = b_combobox.get()
+    name = cryptos[code]
+    b_label.config(text=name)
 
+def update_t_label(event):
+    # Получаем полное название целевой валюты из словаря
+    code = t_combobox.get()
+    name = currencies[code]
+    t_label.config(text=name)
 
+def exchange():
+    crypto_code = b_combobox.get()
+    currency_code = t_combobox.get()
 
-response = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,'
-                      'litecoin,tether&vs_currencies=usd,rub,eur,cny')
-response.raise_for_status()
-data = response.json()
+    if crypto_code and currency_code:
+        try:
+            response = requests.get(
+                f'https://api.coingecko.com/api/v3/simple/price?ids={crypto_code}&vs_currencies={currency_code}'
+            )
+            response.raise_for_status()  # Проверка ошибок HTTP
+            data = response.json()
 
+            if currency_code.lower() in data.get(crypto_code, {}):
+                rate = data[crypto_code][currency_code.lower()]
+                crypto_name = cryptos[crypto_code]
+                currency_name = currencies[currency_code]
+                mb.showinfo("Курс обмена", f"1 {crypto_name} = {rate} {currency_name}")
+            else:
+                mb.showerror("Ошибка", "Данные не найдены")
+        except Exception as e:
+            mb.showerror("Ошибка", f"Ошибка API: {e}")
+    else:
+        mb.showwarning("Внимание", "Выберите криптовалюту и валюту")
 
 # Создание графического интерфейса
 window = Tk()
